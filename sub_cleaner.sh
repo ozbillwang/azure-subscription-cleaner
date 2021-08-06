b6bc2daa43d27eff4955fb2cdfce88f1af72f08c
bill <ozbillwang@gmail.com>
2021-08-06 23:43:03 +1000

    adjust the delete sequency

--- sub_cleaner.sh
+++ sub_cleaner.sh
@@ -14,20 +14,20 @@ read -p "Confirm the account and press any key to resume ..."
 
 id=$(az account show |jq -r '.id')
 
-# Get all resource groups
+# clean blueprint assignments
+assignments=$(az blueprint assignment list --subscription ${id} |jq -r '.[].name')
+for assignment in ${assignments}
+do
+  az blueprint assignment delete --name ${assignment} --subscription ${id} --yes
+done
+
+# Get all resource groups 
 groups=$(az group list --subscription ${id} |jq -r '.[].name')
 
 # clean resource groups
 for group in ${groups}
 do
   az group delete --name ${group} --subscription ${id} --yes --no-wait
-done
-
-# clean blueprint assignments
-assignments=$(az blueprint assignment list --subscription ${id} |jq -r '.[].name')
-for assignment in ${assignments}
-do
-  az blueprint assignment delete --name ${assignment} --subscription ${id} --yes
 done
 
 # clean deleted vaults
